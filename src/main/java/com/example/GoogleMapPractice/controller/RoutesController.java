6b75f84c42a41643be10105d78aa9f4fe28f0b3c
신성규 <kxu4583@naver.com>
2024-01-27 21:39:56 +0900

    feat: url을 원본 프로젝트와 통일

--- src/main/java/com/example/GoogleMapPractice/controller/RoutesController.java
+++ src/main/java/com/example/GoogleMapPractice/controller/RoutesController.java
@@ -5,10 +5,12 @@ import com.example.GoogleMapPractice.service.RoutesService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
 @RestController
+@RequestMapping("/api/v1/routes")
 public class RoutesController {
 
     private final RoutesService routesService;
